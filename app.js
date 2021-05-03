1507d5663a2f2471c3b5b9c2ad137d6a0df0feeb
Красниковская Екатерина <e.krasnikovskaya@qvant.ru>
2021-05-04 00:31:41 +0300

    finish error and success addings

--- app.js
+++ app.js
@@ -16,20 +16,29 @@ class Report {
 
         return [date, time];
     }
+    randomID() {
+        return Math.random()
+        .toString(36)
+        .substring(2, 15);
+    }
+    addLine() {
+        this.addToFile('\n<hr>');
+    }
     // appends file with new lines
     addToFile(lines) {
-        fs.appendFile(
+        fs.appendFileSync(
             this.file, lines,
             function(err) {
               if (err) {
                 return console.log(err);
               }
-              console.log(`${this.getTime()[1]}  Cannot write to file`);
+              console.log('err');
             },
           );
     }
     // writes new file 
     startTheReport(title, script, css) {
+        fs.unlinkSync(this.file); // remove previous file
         this.addToFile(`<!DOCTYPE html>\n
         <html>\n
         <head>\n
@@ -38,37 +47,40 @@ class Report {
         </title>\n
         </head>\n
         <body>\n
-        <script>${script}</script>
+        <script src="${script}"></script>
         <link rel="stylesheet" href="${css}">
         <p><h3>Started the testing</h3></p>\n
-        <p>${this.getTime()[0]}</p>`);
+        <p><h3>${this.getTime()[0]}</h3></p><hr>`);
     }
     // adds an error message to body with displayable stacktrace and optional screenshot
     addError(message, stacktrace, image=undefined) {
+        const idStacktrace = this.randomID();
+        const idScreenshot = this.randomID();
         this.addToFile(
-            `\n<p><div class="message error">${message}</div></p>\n`);
-        this.addToFile(
-            `<p><button class="show-error" onclick="hideAndShow('stacktrace')">See stacktrace</button>`);
+            `\n<p><div class="message error">${this.getTime()[1]} ${message}</div></p>\n`);
+        // add buttons and diagnostic stuff, don't touch for God's sake
         if (image !== undefined) {
             this.addToFile(
-            `<button class="show-image" onclick="hideAndShow('screenshot')">See screenshot</button></p>`);
-        }
-        this.addToFile(
-            `<div id="stacktrace" class ="message" style="display: none">
-            <p>${stacktrace}</p>
-            </div>`);
-        if (image !== undefined) {
-            this.addToFile(
-                `<div id="screenshot" style="display: none">
+            `\n<p><button class="show-error" onclick="hideAndShow('${idStacktrace}')">See stacktrace</button>
+            <button class="show-image" onclick="hideAndShow('${idScreenshot}')">See screenshot</button></p>
+            \n<div id="${idStacktrace}" class ="message" style="display: none">
+                <p>${stacktrace}</p>
+                </div>
+                \n<div id="${idScreenshot}" style="display: none">
                  <p><img src="${image}"></p>
-                 </div>`)
+                 </div>`);
+        } else {
+            this.addToFile(
+                `\n<button class="show-error" onclick="hideAndShow('${idStacktrace}')">See stacktrace</button>
+                \n<div id="${idStacktrace}" class ="message" style="display: none">
+                <p>${stacktrace}</p>
+                </div>`);
         }
-    }
-     // adds just a line
-    addMessage(message) {
     }
      // says that test passed
     addSuccess(message) {
+        this.addToFile(`\n<div class="message success">${this.getTime()[1]} 
+        ${message} &#9989;</div>`)
     }
     // just adds ending
     addEnding() {
@@ -76,4 +88,4 @@ class Report {
     }
 }
 
-export { Report };
+module.exports = { Report };
