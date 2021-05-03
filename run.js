40492b49543e34c4c89cee6a4b1a3b319a889e3f
Красниковская Екатерина <e.krasnikovskaya@qvant.ru>
2021-05-04 00:34:11 +0300

    add script for using example

--- run.js
+++
@@ -1,4 +0,0 @@
-const app = require('./app');
-
-const rep = app.generateReport('title', 'script.js', 'css.css', 'body.html');
-console.log(rep);
---
+++ usingExample.js
@@ -0,0 +1,10 @@
+const {Report} = require('./app');
+
+const report = new Report('./report/toxic-report.html')
+
+report.startTheReport('Toxic', 'hello.js', 'style.css');
+report.addSuccess('Lets pretend it all works');
+report.addSuccess('And add another success message');
+report.addError('Oops! Something imaginary failed', 'blabla stacktrace', 'cover1.jpeg');
+report.addSuccess('God I hope this works');
+report.addEnding();
