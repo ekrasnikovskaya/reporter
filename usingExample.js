2aad64d763355d7423ba6f83dc93264fe620656d
Красниковская Екатерина <e.krasnikovskaya@qvant.ru>
2021-05-04 01:30:48 +0300

    rename js file

--- usingExample.js
+++ usingExample.js
@@ -2,7 +2,7 @@ const {Report} = require('./app');
 
 const report = new Report('./report/toxic-report.html')
 
-report.startTheReport('Toxic', 'hello.js', 'style.css');
+report.startTheReport('Toxic', 'button.js', 'style.css');
 report.addSuccess('Lets pretend it all works');
 report.addSuccess('And add another success message');
 report.addError('Oops! Something imaginary failed', 'blabla stacktrace', 'cover1.jpeg');
